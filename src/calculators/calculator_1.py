b9593228fe79b4fc2a8679e6f0e39236f56946e4
leoneville <leoneville_@hotmail.com>
2024-09-06 12:25:32 -0300

    feat: Implement centralized error handling for calculators
    Standardized error management across all calculator routes by introducing a dedicated error handler. This ensures consistent and informative error responses, improving the robustness and maintainability of the API.

--- src/calculators/calculator_1.py
+++ src/calculators/calculator_1.py
@@ -1,6 +1,8 @@
 from typing import Dict
 from flask import request as FlaskRequest
 
+from src.errors.http_unprocessable_entity import HttpUnprocessableEntityError
+
 
 class Calculator1:
     def calculate(self, request: FlaskRequest) -> Dict:  # type: ignore
@@ -18,7 +20,7 @@ class Calculator1:
 
     def __validate_body(self, body: Dict) -> float:
         if "number" not in body or not isinstance(body["number"], (int, float)):
-            raise Exception("body mal formatado!")
+            raise HttpUnprocessableEntityError("body mal formatado!")
 
         input_data = body["number"]
         return input_data
--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from src.errors.error_controller import handle_errors
 from src.main.factories.calculator1_factory import calculator1_factory
 from src.main.factories.calculator2_factory import calculator2_factory
 from src.main.factories.calculator3_factory import calculator3_factory
@@ -8,20 +9,32 @@ calc_route_bp = Blueprint("calc_routes", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
-    calc = calculator1_factory()
-    response = calc.calculate(request)
-    return jsonify(response), 200
+    try:
+        calc = calculator1_factory()
+        response = calc.calculate(request)
+        return jsonify(response), 200
+    except Exception as exception:
+        error_response = handle_errors(exception)
+        return jsonify(error_response)
 
 
 @calc_route_bp.route("/calculator/2", methods=["POST"])
 def calculator_2():
-    calc = calculator2_factory()
-    response = calc.calculate(request)
-    return jsonify(response), 200
+    try:
+        calc = calculator2_factory()
+        response = calc.calculate(request)
+        return jsonify(response), 200
+    except Exception as exception:
+        error_response = handle_errors(exception)
+        return jsonify(error_response)
 
 
 @calc_route_bp.route("/calculator/3", methods=["POST"])
 def calculator_3():
-    calc = calculator3_factory()
-    response = calc.calculate(request)
-    return jsonify(response), 200
+    try:
+        calc = calculator3_factory()
+        response = calc.calculate(request)
+        return jsonify(response), 200
+    except Exception as exception:
+        error_response = handle_errors(exception)
+        return jsonify(error_response)
